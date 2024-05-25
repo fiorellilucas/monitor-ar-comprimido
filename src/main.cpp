c46f16c8b331c443f5ceb0cfed9b052d4929361f
Lucas <fiorellilucas@gmail.com>
2024-05-25 11:59:50 -0300

    Conectar ao WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiCredentials.h>
+#include <HTTPClient.h>
 
 const int sensorGPIO = 36;
+const char* ssid = SSID;
+const char* senha = SENHA;
+
 int valorSensor = 0;
+
+void conectarWifi(void) {
+    WiFi.begin(ssid, senha);
+    Serial.println("Conectando");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println(".");
+    }
+    Serial.println("Wifi conectado");
+}
 
 void setup() {
     Serial.begin(9600);
+    conectarWifi();
 }
 
 void loop() {
-    valorSensor = analogRead(sensorGPIO);
-    Serial.println(valorSensor);
-    delay(100);
+    // valorSensor = analogRead(sensorGPIO);
+    // // Serial.println(valorSensor);
+    // // delay(100);
 }
